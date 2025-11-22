6fca710550949f8b9605e858e7394891aa247be9
hddgpp <youssefhehe3@gmail.com>
2025-11-22 01:29:06 +0100

    Creating the checkout page

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import {Routes, Route} from 'react-router'
 import './App.css'
 import Home from './components/Home'
+import Checkout from './components/Checkout'
 
 function App() {
   return (
     <>
-     <Home/>
+      <Routes>
+        < Route index element={ <Home/>}/>
+        < Route path='checkout' element={ <Checkout/>}/>
+      </Routes>
     </>
   )
 }
